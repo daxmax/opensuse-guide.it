678951ea6faef5ffabf3ecb44b0c0af89c87488d
Massimo D'Alessio <massimo.dalessio@gmail.com>
2016-12-04 10:50:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,35 @@
 <?php include 'header.php'; ?>
-<title>1. Introduction - Beginner's Guide to openSUSE Leap 42.2 / SUSE Linux</title>
+<title>1. Introduzione - Guida per principianti a openSUSE Leap 42.2 / SUSE Linux</title>
 <?php include 'header2.php'; ?>
 <?php include 'menu.php'; ?> 
 
 
 
-<div class="heading1">1. Introduction</div>
-The purpose of this guide is to provide new users with everything they need to know to get started using openSUSE Leap as a home user/small office desktop operating system - quickly and easily. <br /><br /> 
+<div class="heading1">1. Introduzione</div>
+Lo scopo di questa guida è di fornire ai nuovi utenti tutte le informazioni necessarie per iniziare ad usare openSUSE Leap come un sistema operativo casalingo e/o per uso di ufficio.<br /><br /> 
 
-Reading this guide should save you <i>a lot</i> of time in the long run. The appendix at the end is targetted at particularly enthusiastic readers.<br /><br />
+Leggere questa breve guida vi fornirà le informazioni basilari che vi permetteranno di risparmiare tempo in futuro. L’appendice è rivolta a utenti più avanzati.<br /><br />
 
-Note that this guide is not officially related to the <a href="http://www.opensuse.org" target="_blank">openSUSE project</a> nor <a href="http://www.suse.com" target="_blank">SUSE LINUX Products GmbH</a> nor Micro Focus International.<br /><br />
+N.B. Questa guida non è ufficiale e non è collegata all’<a href="http://www.opensuse.org" target="_blank">openSUSE project</a>,a <a href="http://www.suse.com" target="_blank">SUSE LINUX Products GmbH</a> e Micro Focus International.<br /><br />
 
-Have a lot of fun!<br /><br />
-
-
-
-<div class="heading2">1.1 About openSUSE Leap</div>
-openSUSE Leap is a PC operating system based on GNU and Linux. It's a free/open source and gratis alternative to e.g. Microsoft Windows with many advantages. openSUSE Leap is suitable for laptops, desktops, netbooks, servers and multimedia center PCs at home or in small offices.<br /><br />
-
-openSUSE Leap is among the leading GNU/Linux distributions and is also one of the oldest existing ones. openSUSE Leap uses a core system from SUSE Linux Enterprise but with up-to-date hardware support, desktop environments and graphical applications. Every year a major service pack is released and every 3-4 years there is a major new release where also the core system will be upgraded.<br /><br />
-
-For experienced and adventurous GNU/Linux users who want to take risks with their operating system and live on the bleeding edge, there is also a rolling release version of openSUSE available called <a href="https://en.opensuse.org/Portal:Tumbleweed" target="_blank">openSUSE Tumbleweed</a><br /><br />
-
-openSUSE Leap is developed openly and transparently by community volunteers working together with SUSE employees.<br /><br />
+Buona lettura!<br /><br />
 
 
 
-<div class="heading2">1.2 This Guide in Other Languages (Derivative Works)</div>
+<div class="heading2">1.1 openSUSE Leap</div>
+openSUSE Leap è un sistema operativo per PC basato su GNU e Linux. E’ un’alternativa open source a Microsoft Windows che presenta molti vantaggi. openSUSE Leap è utilizzabile su computer laptop, desktop, notebook, server e PC multimediali utilizzati sia a casa che in ufficio.<br /><br />
+
+OpenSUSE Leap è una distribuzione leader nel panorama GNU/Linux oltre ad essere una delle più longeve.<br />
+openSUSE Leap utilizza il core del sistema SUSE Linux Enterprise aggiungendo un migliore supporto hardware, un migliore ambiente desktop e ricche applicazioni grafiche.<br/>
+Il progetto prevede un aggiornamento principale della distribuzione ogni anno e, ogni 3-4 anni, verrà anche aggiornato il core basato su SUSE Linux Enterprise.<br /><br />
+
+Gli utenti esperti e appassionati di GNU/Linux, che preferiscono utilizzare sempre le applicazioni più aggiornate, possono optare per la rolling release di openSUSE chiamata <a href="https://en.opensuse.org/Portal:Tumbleweed" target="_blank">openSUSE Tumbleweed</a>.<br /><br />
+
+openSUSE Leap è sviluppata in maniera aperta e trasparente dai volontari della comunità che lavorano insieme agli impiegati SUSE.<br /><br />
+
+
+
+<div class="heading2">1.2 Questa guida è disponibile anche in altre lingue (Derivative Works)</div>
 <ul>
 <li><a href="http://dafter.co.nf/wp/" target="_blank">Arabic by Sadig Osman</a></li>
 <li><a href="https://victorhck.gitlab.io/guia_openSUSE/" target="_blank">Spanish by victorhck</a></li>
