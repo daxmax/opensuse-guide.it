616c78c439dc6b70537e9cd700bb7ec0645368fe
Massimo D'Alessio <massimo.dalessio@gmail.com>
2016-12-07 22:22:14 +0100

    Update apps.php

--- apps.php
+++ apps.php
@@ -1,67 +1,67 @@
 <?php include 'header.php'; ?>
-<title>6. Apps for Common Tasks - Applications to Perform the Most Common Tasks</title>
+<title>6. Applicazioni per utilizzi comuni - Applications to Perform the Most Common Tasks</title>
 <?php include 'header2.php'; ?>
 <?php include 'menu.php'; ?> 
 
 
 
-<div class="heading1">6. Apps for Common Tasks</div>
-Here's a list of applications used to perform common tasks, they're all installed by default and are all high quality, but naturally you can replace them with alternatives if you wish.<br /><br />
+<div class="heading1">6. Applicazioni per utilizzi comuni</div>
+Ecco una lista di applicazioni utilizzabili per gli usi più comuni. Sono tutte installate di default e sono tutte complete e di qualità. Ovviamente potrai sostituirle in qualsiasi momento con le tue applicazioni preferite.<br /><br />
 
 <div class="tip">
 <table>
 <tbody>
 <tr>
 <td><img src="images/pics/tip.png" alt="tip" /></td>
-<td>All the alternatives mentioned below will be available for easy installation with the <a href="installpackage.php">package manager</a>, if the recommended <a href="repositories.php">software repositories</a> are added (see later chapters).</td>
+<td>Tutte le alternative menzionate sono installabili utilizzando il <a href="installpackage.php">gestore dei pacchetti</a>, dopo aver aggiunto i <a href="repositories.php">repositories</a> della comunità (vedi i capitoli successivi).</td>
 </tr>
 </tbody>
 </table>
 </div><br />
 
-You can find more replacements for your familiar applications here:<br />
+Trovi ulteriori alternative alle applicazioni di default qui:<br />
 <a href="http://en.opensuse.org/Application_equivalents" target="_blank">http://en.opensuse.org/Application_equivalents</a><br /><br />
 
 
 
-<div class="heading2">6.1 Surf the Web with Firefox</div>
-Mozilla Firefox is the default web browser. It's one of the world's leading web browsers.<br /><br />
+<div class="heading2">6.1 Naviga il Web con Firefox</div>
+Mozilla Firefox è il web browser di default. E' uno dei migliori web browsers al mondo.<br /><br />
 
 <center><a href="images/screenshots/firefox.png" rel="thumbnail"><img src="images/screenshots/firefoxb.png" alt="firefox" class="pic" /></a></center><br />
 
-Alternatives to consider: Rekonq, Opera, Chromium, Konqueror<br /><br />
+Puoi provare anche: Rekonq, Opera, Chromium, Konqueror<br /><br />
 
 
 
-<div class="heading2">6.2 Office Productivity with LibreOffice</div>
-LibreOffice is a complete office suite with word processing, spreadsheets, presentation, drawing and more. It's very compatible with Microsoft Office.<br /><br />
+<div class="heading2">6.2 LibreOffice, il software libero di produttività personale</div>
+LibreOffice e una suite di programmi di produttività personale che comprende un word processor, un foglio di calcolo, un programma per presentazioni multimediali, un database, un programma di disegno e altro ancora. E' compatible con Microsoft Office.<br /><br />
 
 <center><a href="images/screenshots/lowriter.png" rel="thumbnail"><img src="images/screenshots/lowriterb.png" alt="libreoffice" class="pic" /></a></center><br />
 
-Alternatives to consider: Calligra, Abiword, Gnumeric<br /><br />
+Puoi provare anche: Calligra, Abiword, Gnumeric<br /><br />
 
 
 
-<div class="heading2">6.3 Listen to Music with Amarok</div>
-Amarok is a feature rich music player that handles your music, podcasts, portable music player or iPod etc. Read about codec installation in a later chapter.<br /><br />
+<div class="heading2">6.3 Ascolta la tua Musica con Amarok</div>
+Amarok è un completo player musicale con cui puoi gestire la tua musica, i tuoi podcast, i tuoi lettori multimediali, iPod ecc. Leggi come abilitare i codec in un prossimo capitolo.<br /><br />
 
 <center><a href="images/screenshots/amarok.png" rel="thumbnail"><img src="images/screenshots/amarokb.png" alt="amarok" class="pic" /></a></center><br />
 
-Alternatives to consider: Clementine, JuK, Qmmp<br /><br />
+Puoi provare anche: Clementine, JuK, Qmmp<br /><br />
 
 
 
-<div class="heading2">6.4 Watch Video with Dragon Player</div>
-Dragon Player plays videos and DVDs. Read about codec installation in a later chapter.<br /><br />
+<div class="heading2">6.4 Guarda i tuoi Video con Dragon Player</div>
+Dragon Player visualizza file video e DVD. Leggi come abilitare i codec in un prossimo capitolo.<br /><br />
 
 <center><a href="images/screenshots/dragon.png" rel="thumbnail"><img src="images/screenshots/dragonb.png" alt="vlc" class="pic" /></a></center><br />
 
-Alternatives to consider: VLC, SMPlayer, mpv<br /><br />
+Puoi provare anche: VLC, SMPlayer, mpv<br /><br />
 
 
 
-<div class="heading2">6.5 Image Editing with GIMP</div>
-GIMP (GNU Image Manipulation Program) is a very powerful image editing application.<br /><br />
+<div class="heading2">6.5 Elabora le immagini con GIMP</div>
+GIMP (GNU Image Manipulation Program) è un potente software per l'elaborazione digitale delle immagini.<br /><br />
 
 <center><a href="images/screenshots/gimp.png" rel="thumbnail"><img src="images/screenshots/gimpb.png" alt="gimp" class="pic" /></a></center><br />
 
@@ -70,22 +70,22 @@ GIMP (GNU Image Manipulation Program) is a very powerful image editing applicati
 <tbody>
 <tr>
 <td><img src="images/pics/tip.png" alt="tip" /></td>
-<td>GIMP can work as a single window, simply click 'Windows' in the menu and check the option 'Single-Window Mode'.</td>
+<td>GIMP può lavorare in una singola finestra, basta cliccare 'Windows' nel menù e selezionare l'opzione 'Single-Window Mode'.</td>
 </tr>
 </tbody>
 </table>
 </div><br />
 
-Alternatives to consider: Gwenview (only very basic editing)<br /><br />
+Puoi provare anche: Gwenview (only very basic editing)<br /><br />
 
 
 
-<div class="heading2">6.6 E-mail, Calendar, News feeds and more with Kontact</div>
-Kontact is a fullblown PIM (personal information management) suite, combining e-mail, calendar, news feeds, addressbook and more in one place. Each of the Kontact components (KMail, KOrganizer, Akregator etc.) can also be used as standalone applications.<br /><br />
+<div class="heading2">6.6 E-mail, Calendario, News feeds e tanto altro con Kontact</div>
+Kontact è una suite di programmi per PIM (personal information management). Gestisce posta elettronica, calendari, contatti, liste di cose da fare. Le singole applicazioni (KMail, KOrganizer, Akregator) possono essere utilizzate come singole applicazioni.<br /><br />
 
 <center><a href="images/screenshots/kontact.png" rel="thumbnail"><img src="images/screenshots/kontactb.png" alt="kontact" class="pic" /></a></center><br />
 
-Alternatives to consider: Mozilla Thunderbird<br /><br />
+Puoi provare anche: Mozilla Thunderbird<br /><br />
 
 
 
